13e1ae93ba56b3e5c4be9585cf1ddba480ac3b8c
Saienko Vladyslav <109801320+Vladyslav44@users.noreply.github.com>
2022-09-04 03:33:25 +0300

    edit structure

---
+++ assets/scripts/router.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#app').prepend(`     
+      <div class="pages-router">
+        <a href="/testTaskGoC/first-mockup/index.html">First Mockup</a>
+        <a href="/testTaskGoC/second-mockup/index.html">Second Mockup</a>
+        <a href="/testTaskGoC/third-mockup/index.html">Third Mockup</a>
+        <a href="#app">Up</a>
+      </div>
+	`);
+});
